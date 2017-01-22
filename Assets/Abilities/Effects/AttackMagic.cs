d909b210647c9146c57f287cd8cf06a69e698a1a
Oreshkov Vladimir <voresh@inbox.ru>
2017-01-22 13:14:29 +0300

    magic attack effects added

---
+++ Assets/Abilities/Effects/AttackMagic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.Abilities.Effects
+{
+    public class AttackMagic : Effect
+    {
+        [SerializeField]
+        private int _amount;
+
+        public void Awake()
+        {
+            UpdateValueField(_amount);
+        }
+
+        protected override void AddEffect()
+        {
+            Target.GetDamage(_amount);
+        }
+
+        protected override void RemoveEffect()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
