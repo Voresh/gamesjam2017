805fffcd30d307cc1c54e710761b8b9c5f452887
Oreshkov Vladimir <voresh@inbox.ru>
2017-01-22 14:09:43 +0300

    shadow dev started

---
+++ Assets/ShadowManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets
+{
+    public class Shadow : MonoBehaviour
+    {
+        [SerializeField]
+        private Image[] _heroShadows;
+        [SerializeField]
+        private Image[] _museShadows;
+    }
+}
